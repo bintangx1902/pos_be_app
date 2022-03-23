c769acdac3bebe4805f60327ccb511406a65c308
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-03-23 20:24:48 +0700

    add allauth to projects

--- a_pos/settings.py
+++ a_pos/settings.py
@@ -39,9 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 
     'rest_framework',
     'pos',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 MIDDLEWARE = [
