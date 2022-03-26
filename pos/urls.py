ddffdce1a21b5e1745fcf383a2db18cfe484c81f
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-03-26 16:00:20 +0700

    views and serializer

--- pos/urls.py
+++ pos/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('product', ProductAPI.as_view(), name='product-end-point'),
+    path('category', CategoryAPI.as_view(), name='category-end-point'),
 ]
--- pos/views.py
+++ pos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializrers import *
+from rest_framework import status
 
 
 class ProductAPI(APIView):
@@ -11,4 +12,35 @@ class ProductAPI(APIView):
         return Response(serializer.data)
 
     def post(self, format=None):
+        serializer = ProductSerializer(data=self.request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status.HTTP_400_BAD_REQUEST)
+
+
+class OrderItemAPI(APIView):
+    def get(self, format=None):
+        item = OrderItem.objects.all()
+        serializer = OrderItemSerializer(item, many=True)
+        return Response(serializer.data)
+
+    def post(self):
         return
+
+
+class CategoryAPI(APIView):
+    def get(self, format=None):
+        cate = Category.objects.all()
+        serializer = CategorySerializer(cate, many=True)
+        return Response(serializer.data)
+
+    def post(self, format=None):
+        name = self.request.data['name']
+        category = Category()
+        category.cate = name
+        category.save()
+
+        cate = Category.objects.all()
+        serializer = CategorySerializer(cate, many=True)
+        return Response(serializer.data)
