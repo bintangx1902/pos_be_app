0e588a40c85a52bc0b93e333d4f84417ad2e000f
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-03-26 12:56:16 +0700

    registering to admin

--- pos/admin.py
+++ pos/admin.py
@@ -1,5 +1,9 @@
 from django.contrib.admin import site
-from .models import UserProfile
+from .models import UserProfile, Product, OrderItem, Order, Coupon
 
 
 site.register(UserProfile)
+site.register(Product)
+site.register(OrderItem)
+site.register(Order)
+site.register(Coupon)
