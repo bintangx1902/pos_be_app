94a07ccec9b69303ba78c23dd30d0cf78acfdd5d
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-03-26 12:54:00 +0700

    create models

--- pos/models.py
+++ pos/models.py
@@ -6,3 +6,71 @@ class UserProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     profile_img = models.ImageField(upload_to='profile/', null=True, blank=True)
     phone_number = models.CharField(max_length=255)
+
+
+class Category(models.Model):
+    cate = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.cate
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    link = models.SlugField(max_length=255, unique=True)
+    price = models.FloatField(verbose_name='Real Price : ')
+    disc = models.FloatField(null=True, blank=True, verbose_name='Discount price if desire : ')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product/')
+
+    def __str__(self):
+        return f"{self.name} - Rp. {self.price}"
+
+
+class Coupon(models.Model):
+    code = models.SlugField(unique=True)
+    discount = models.IntegerField(verbose_name='discount in percent ')
+
+    def __str__(self):
+        return self.code
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.name}"
+
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+    def get_total_item_discount_price(self):
+        return self.quantity * self.item.disc
+
+    def get_final_price(self):
+        if self.item.disc:
+            return self.get_total_item_discount_price()
+        return self.get_total_item_price()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ManyToManyField(OrderItem)
+    order_date = models.DateTimeField(null=True, blank=True)
+    ordered = models.BooleanField(default=False)
+    coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    def get_total(self):
+        total = 0
+        for item in self.item.all():
+            total += item
+        if self.coupon:
+            discount = self.coupon.discount / 100
+            total -= (total * discount)
+        return total
