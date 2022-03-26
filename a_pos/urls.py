b8ebebe53e30751306800db4460bbe6ba71b4b8d
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-03-26 09:40:00 +0700

    add static and media to project url

--- a_pos/urls.py
+++ a_pos/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('static/<path>', serve, settings.STATIC_ROOT),
+    path('media/<path>', serve, settings.MEDIA_ROOT),
 ]
+
+if not settings.DEBUG or settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
