225cda0fb0b9bfadc79add5aeefc501f5b6d3eab
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-04-03 21:51:44 +0700

    reduce item registered in path

--- demo/urls.py
+++ demo/urls.py
@@ -8,6 +8,7 @@ app_name = 'demo'
 urlpatterns = [
     path('', ShowMenu.as_view(), name='landing'),
     path('cart', OrderedItem.as_view(), name='cart'),
+    path('cart/reduce/<slug:link>', reduce_item, name='reduce'),
     path('cart/remove/<slug:link>', remove_from_cart, name='remove'),
     path('add-item/<slug:link>', AddItem.as_view(), name='add-item'),
 ]
