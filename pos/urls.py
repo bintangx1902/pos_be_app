e8d48ac6b29238a268b5bf9f9eece5866785b0a0
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2022-04-14 15:27:23 +0700

    cart item serializer, update models, and urls

--- pos/urls.py
+++ pos/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import *
 
+app_name = 'pos'
 
-urlpatterns = []
-
-
-urlpatterns += [
+urlpatterns = [
+    path('', api_landing, name='landing'),
+    path('item', CartItem.as_view(), name='cart-item'),
     path('product', ProductAPI.as_view(), name='product-end-point'),
     path('category', CategoryAPI.as_view(), name='category-end-point'),
 ]
--- pos/views.py
+++ pos/views.py
@@ -3,6 +3,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializrers import *
 from rest_framework import status
+from rest_framework.decorators import api_view
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+
+
+@api_view(['GET'])
+def api_landing(request):
+    data = {
+        'product': '/api/product',
+        'category': '/api/category',
+        'cart item': '/api/item',
+    }
+    return Response(data)
 
 
 class ProductAPI(APIView):
@@ -44,3 +59,56 @@ class CategoryAPI(APIView):
         cate = Category.objects.all()
         serializer = CategorySerializer(cate, many=True)
         return Response(serializer.data)
+
+
+class CartItem(APIView):
+    def get(self, format=None, **kwargs):
+        order = Order.objects.filter(user=self.request.user, ordered=False).first()
+        if order:
+            order = order.item.all()
+
+        serializer = OrderItemSerializer(order, many=True)
+        return Response(serializer.data)
+
+    def post(self, format=None, **kwargs):
+        data = self.request.data
+        item = float(data['item'])
+        amount = float(data['amount'])
+        xtra = float(data['xtra'])
+
+        item = get_object_or_404(Product, pk=item)
+
+        if not amount and not xtra:
+            # TODO : add messages
+            return HttpResponseRedirect(reverse('/'))
+        elif not amount and xtra:
+            amount = 1
+        elif not xtra:
+            xtra = 0
+
+        order_item, created = OrderItem.objects.get_or_create(
+            item=item,
+            user=self.request.user,
+            ordered=False
+        )
+
+        order_qs = Order.objects.filter(user=self.request.user, ordered=False)
+        if order_qs.exists():
+            order = order_qs[0]
+            if order.item.filter(item__link=item.link).exists():
+                order_item.quantity += amount
+                order_item.xtra += xtra
+            else:
+                order.item.add(order_item)
+                order_item.quantity = amount
+                order_item.xtra = xtra
+        else:
+            # TODO : Create links
+            order = Order.objects.create(user=self.request.user, order_date=timezone.now(), slug=None)
+            order.item.add(order_item)
+            order_item.quantity = amount
+            order_item.xtra = xtra
+
+        order_item.save()
+        print(f"{item} - {amount} - {xtra}")
+        return HttpResponseRedirect(reverse('pos:cart-item'))
